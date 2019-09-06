ea6f0e5713df5c8769e1021778dc42714915e5bb
BlockWorker <alex.kharitonov.2000@gmail.com>
2019-09-06 14:49:58 +0200

    Added basic MCU program

---
+++ MCU/BlockBox.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=BlockBox.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/BlockBox.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=blockbox.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/blockbox.x.tar
